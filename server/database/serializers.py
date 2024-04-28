0f1ddbe770b3ca9c4f221a4a2f0261075de8b830
Ali Mansour <113988768+A1iMansour@users.noreply.github.com>
2024-04-29 00:54:41 +0300

    Delete server directory

--- server/database/serializers.py
+++
@@ -1,7 +0,0 @@
-import rest_framework.serializers as serializers
-import database.models as models
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.NetworkEntry
-        fields = '__all__' #can change it later
--- server/database/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-   
-    path('',views.home, name='homefunc'),
-    path('send_message/',views.send_message, name='send_message'),
-    path('receive_message/',views.receive_message, name='receive_message'),
-]
